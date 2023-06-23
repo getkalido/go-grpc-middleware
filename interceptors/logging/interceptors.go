91063c04766051066aa4fc6d82990db3e30abe5e
Luther Fourie <luther@kalido.me>
2023-06-23 15:49:40 +0200

    Updated reporter interface for loggin interceptor

--- interceptors/logging/interceptors.go
+++ interceptors/logging/interceptors.go
@@ -134,6 +134,9 @@ func (c *reporter) PostMsgReceive(payload any, err error, duration time.Duration
 	}
 }
 
+// TTFB Tracking&Reporting
+func (c *reporter) PostTTFB(time.Duration) {}
+
 func reportable(logger Logger, opts *options) interceptors.CommonReportableFunc {
 	return func(ctx context.Context, c interceptors.CallMeta) (interceptors.Reporter, context.Context) {
 		kind := KindServerFieldValue
